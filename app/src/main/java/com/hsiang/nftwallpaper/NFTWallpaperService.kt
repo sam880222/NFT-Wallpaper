7ac10d61e084a89bce4a2b5c7919b60eb0c5b1b6
Hsiang <sam880222@gmail.com>
2022-03-26 18:08:44 +0800

    initial

---
+++ app/src/main/java/com/hsiang/nftwallpaper/NFTWallpaperService.kt
@@ -0,0 +1,9 @@
+package com.hsiang.nftwallpaper
+
+import android.service.wallpaper.WallpaperService
+
+class NFTWallpaperService: WallpaperService() {
+    override fun onCreateEngine(): Engine {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/hsiang/nftwallpaper/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.hsiang.nftwallpaper.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
